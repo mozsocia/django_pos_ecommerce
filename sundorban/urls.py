ca1c0196a5358a20635cb7330f0ed74b94d422fa
Sheikh Rokon Uzzman <mdorokon.u545@gmail.com>
2023-08-03 11:11:37 +0600

    add sales page

--- sundorban/urls.py
+++ sundorban/urls.py
@@ -18,8 +18,8 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 
-from django.views.static import serve
-from django.conf.urls import url
+# from django.views.static import serve
+# from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -32,8 +32,8 @@ urlpatterns = [
     path('pos/', include('pos_dashboard.urls')),
     path('oauth/', include('social_django.urls', namespace='social')),  # <-- here
 
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    # url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    # url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 
 ]
 
